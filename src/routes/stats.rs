28a59d3c700c39b0d6a9d8a3a0458dc3c0dfe78b
Timmy Chang <timmy5519@gmail.com>
2024-09-18 18:11:26 +0800

    Improve stats query API

--- src/routes/stats.rs
+++ src/routes/stats.rs
@@ -9,16 +9,24 @@ pub struct Stats {
 }
 
 pub async fn get_stats() -> Result<Json<Stats>, StatusCode> {
+    const TOTAL_NUMBER_OF_QUERIES_ID: i64 = 0;
+    const TOTAL_NUMBER_OF_BLOCKED_ID: i64 = 1;
     let conn = establish_connection()?;
 
     let dns_queries: u64 = conn
-        .query_row("SELECT COUNT(*) FROM queries", [], |row| row.get(0))
+        .query_row(
+            "SELECT value FROM counters WHERE id = ?",
+            [&TOTAL_NUMBER_OF_QUERIES_ID],
+            |row| row.get(0),
+        )
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     let blocked_queries: u64 = conn
-        .query_row("SELECT COUNT(*) FROM queries WHERE status = 1", [], |row| {
-            row.get(0)
-        })
+        .query_row(
+            "SELECT COUNT(*) FROM queries WHERE status = ?",
+            [&TOTAL_NUMBER_OF_BLOCKED_ID],
+            |row| row.get(0),
+        )
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     let stats = Stats {
